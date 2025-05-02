47c501a24504acc7255fa14941e58cd847ed8d1a
Mmm661-jpeg <Mmmohamed451@gmail.com>
2025-05-02 15:56:35 +0200

    fixed a 'regexerror'

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/airhockey-front/dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*any}', (req, res) => {
 res.sendFile(path.join(__dirname, '../client/airhockey-front/dist', 'index.html'));
 });
 
